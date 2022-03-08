43741ca7a8a1c30cdc690db330f9c10d2aae335b
dchyii <hongyichong@gmail.com>
2022-03-08 10:00:14 +0800

    updated create case api

--- cases/views.py
+++ cases/views.py
@@ -72,9 +72,19 @@ class DashboardCasesView(ListCreateAPIView):
     def get_queryset(self):
         return Case.objects.filter(current_step_user=self.request.user)
 
-    def perform_create(self, serializer):
-        serializer.save(staffer=self.request.user)
-    
+    # def perform_create(self, serializer):
+    #     serializer.save(staffer=self.request.user)
+    def create(self, request, *args, **kwargs):
+        case_data = request.data
+
+        new_case = Case.objects.create(title=case_data["title"], value=case_data["value"], folder_link=case_data["folder_link"], staffer = User.objects.get(id=case_data["staffer"]), current_status = case_data["current_status"], current_step = case_data["current_step"], current_step_user=User.objects.get(id=case_data["current_step_user"]), next_step=case_data["next_step"], next_step_user=User.objects.get(id=case_data["next_step_user"]), planning=Planning.objects.get(id=case_data["planning"]),bidding=Bidding.objects.get(id=case_data["bidding"]), approval=Approval.objects.get(id=case_data["approval"]), contracting=Contracting.objects.get(id=case_data["contracting"]))
+
+        new_case.save()
+
+        serializer = CaseSerializer(new_case)
+
+        return Response(serializer.data)
+
 class DashboardCasesDetailedView(RetrieveUpdateDestroyAPIView):
     serializer_class = DashboardSerializer
     permission_classes = [permissions.IsAuthenticated]
